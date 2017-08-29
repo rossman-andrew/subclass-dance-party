222f70110549493fc5719025069720f5814ded80
Christie Villanueva <christiejvillanueva@gmail.com>
2017-08-29 08:48:34 -0700

    Refactor adding image to catDancer
    Use jQuery methods instead of modifying node directly

--- src/catDancer.js
+++ src/catDancer.js
@@ -1,7 +1,7 @@
 var makeCatDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
-  this.$node = $('<span class="catDancer"><img src="./img/cat.gif"></span>');
-  makeDancer.prototype.setPosition.call(this, this.top, this.left);
+  $imgnode = $('<img src="./img/cat.gif"></img>');
+  $(this.$node).append($imgnode).addClass('catDancer');
 };
 
 makeCatDancer.prototype = Object.create(makeDancer.prototype);
@@ -11,11 +11,14 @@ makeCatDancer.prototype.oldStep = function() {
   makeDancer.prototype.step.call(this);
 };
 
-
 makeCatDancer.prototype.step = function() {
   this.oldStep();
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
-  this.$node.slideToggle('slow');
+   // this.$node.animate({rotate: 360deg});
+};
+
+makeCatDancer.prototype.lineUp = function() {
+  this.$node.addClass('leftAlign');
 };
